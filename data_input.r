fa6456c1fb731a8f917b8de9aef64ce887bd0532
Paula <pmunoz06@ucm.es>
2019-03-21 18:17:19 +0100

    Update data_input.r
    Función get_existing_words devuelve las palabras de una lista que están en el diccionario, fu
    nciona

--- data_input.r
+++ data_input.r
@@ -1,39 +1,60 @@
 install.packages("stringr")
 install.packages("rvest")
-install.packages("tm") # Si no funciona la instalación, probar instalando antes el paquete XML
+install.packages("tm") # Si no funciona la instalaciÃ³n, probar instalando antes el paquete XML
 install.packages("SnowballC")
 install.packages("rlist")
 install.packages("hash")
+install.packages("qdapDictionaries")
+
 library(tm)
 library(hash) #para crear y operar con estructuras hash
 library(stringr)
 library(stringi)
+library(qdapDictionaries)
 
 #primer dataset: songdata.csv
-ASCL <- read.csv(paste(getwd(), "/Data/songlyrics/songdata.csv", sep = ""), header = TRUE, sep = ",", nrows = 5, colClasses = c(NA, NA, "NULL", NA))
+ASCL <- read.csv(paste(getwd(), "/songdata.csv", sep = ""), header = TRUE, sep = ",", colClasses = c(NA, NA, "NULL", NA))
 #la tercera columna son las letras
 names(ASCL)[3] <- "lyrics"
 #segundo dataset: lyrics.csv
-aux <- read.csv(paste(getwd(), "/Data/songlyrics/lyrics.csv", sep = ""), header = TRUE, sep = ",", nrows = 5, colClasses = c("NULL", NA, "NULL", NA, "NULL", NA))
+aux <- read.csv(paste(getwd(), "/lyrics.csv", sep = ""), header = TRUE, sep = ",",  colClasses = c("NULL", NA, "NULL", NA, "NULL", NA))
 #mismo orden de columas que ASCL1
 aux <- aux[,c(2, 1, 3)]
 #concatenar los data frames
 ASCL <- rbind(ASCL, aux)
 
-# Convertimos a minúsculas cada palabra, no solo de la letra de las canciones, sino del nombre del grupo y canción
-#Problema -> Al hacer apply devuelve una lista en vez de un data.frame, así que volvemos a convertirlo a data.frame
-# la función t() devuelve la matriz transpuesta, ya que por alguna razón apply devuelve las columnas como filas y al revés
+# Convertimos a minÃºsculas cada palabra, no solo de la letra de las canciones, sino del nombre del grupo y canciÃ³n
+#Problema -> Al hacer apply devuelve una lista en vez de un data.frame, asÃ­ que volvemos a convertirlo a data.frame
+# la funciÃ³n t() devuelve la matriz transpuesta, ya que por alguna razÃ³n apply devuelve las columnas como filas y al revÃ©s
 ASCL <- data.frame(t(apply(ASCL, 1, tolower))) #stopwords estan en minuscula
 ASCL[,3] <- removeWords(as.character(ASCL[,3]), words = stopwords("english")) 
 ASCL[,3] <- stripWhitespace(ASCL[,3])
 ASCL[,3] <- removePunctuation(ASCL[,3])
 ASCL[,3] <- removeNumbers(ASCL[,3]) # Por si acaso
-for(i in 1:length(ASCL))
-  ASCL[i] <- lapply(ASCL[i], toupper) #pasar el dataframe a mayusc
+#ASCL[,3] <- iconv(ASCL[,3], to="ASCII//TRANSLIT") #Quitamos las tildes
+#ASCL[,3] <- str_remove_all(ASCL[, 3], "[0-9]")
+#ASCL[,3] <- str_remove_all(ASCL[, 3], "[[:punct:]]")
+#ASCL[,3] <- gsub('([[:alpha:]])\\1+', '\\1', ASCL[,3]) #Eliminamos las repeticiones de letras en cada palabra
 
 #############################################################################
+#Comprobamos si la palabra está en el diccionario
+is.word  <- function(x) x %in% GradyAugmented
+get_existing_words <- function(x){ #No hace nada por el for, x no es una lista?
+  lyric <- list()
+  for (w in unlist(stri_extract_all_words(x))) {
+    if (is.word(w)){
+      lyric <- c(lyric, w)
+    }
+  }
+  return(lyric)
+}
+ASCL[,3] <- get_existing_words(ASCL[,3]) #Esta línea debería sustituir todas las de arriba, además comprueba palabras en inglés y castellano siempre en minusuclas
+
+for(i in 1:length(ASCL))
+  ASCL[i] <- lapply(ASCL[i],toupper) #pasar el dataframe a mayusc
+#############################################################################
 #dividir la string que contiene la letra en palabras
-words = unlist(stri_extract_all_words(ASCL[[3]][1]))
+words = unlist(stri_extract_all_words(ASCL[[3]]))
 #esta es la idea aunque no funciona:
 for(i in 1:length(ASCL[[3]])){
   words <- unlist(stri_extract_all_words(ASCL[[3]][i]))
@@ -53,3 +74,6 @@ o <- hash(auths, c(h, h, h, h, h))
 #VISUALIZACION DE DATOS
 auths_count <- as.data.frame(table(ASCL$artist)) #contabilidad de el numero de veces que aparece cada autor en el dataframe
 plot(auths_count) #visualizacion de los datos antes de agrupar
+
+#Visualización de las 10 palabras más populares
+words.freq <- as.data.frame(table(words))
