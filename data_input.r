8e17c0f76af9b4f64dd0d7c1498fcd61b974092c
Uribus <jdrecont25@gmail.com>
2019-04-03 19:42:34 +0200

    Change
    While creating the structure, we now leave the artist colum of the data frame as it is so that when we browse the url is right.
    Added the wordcloud function.
    Included the new dataframe that will contain the country of each artist, also with its frequency.

--- data_input.r
+++ data_input.r
@@ -3,12 +3,16 @@ install.packages("rvest")
 install.packages("tm") # Si no funciona la instalaciÃ³n, probar instalando antes el paquete XML
 install.packages("SnowballC")
 install.packages("rlist")
-install.packages("hash")
+#install.packages("hash")
 install.packages("ngram")
 install.packages("qdapDictionaries")
+install.packages("wordcloud")
+install.packages("RColorBrewer")
 
+library(RColorBrewer)
+library(wordcloud)
 library(tm)
-library(hash) #para crear y operar con estructuras hash
+#library(hash) #para crear y operar con estructuras hash
 library(stringr)
 library(stringi)
 library(ngram)
@@ -32,22 +36,22 @@ get_existing_words <- function(x){ #Tarda mucho :(
 ##############################################################################################################################
 
 #primer dataset: songdata.csv
-ASCL <- read.csv(paste(getwd(), "/songdata.csv", sep = ""), header = TRUE, sep = ",", nrows = 200, colClasses = c(NA, NA, "NULL", NA))
+ASCL <- read.csv(paste(getwd(), "/Data/songlyrics/songdata.csv", sep = ""), header = TRUE, sep = ",", nrows = 200, colClasses = c(NA, NA, "NULL", NA))
 #la tercera columna son las letras
 names(ASCL)[3] <- "lyrics"
 #segundo dataset: lyrics.csv
-aux <- read.csv(paste(getwd(), "/lyrics.csv", sep = ""), header = TRUE, sep = ",", nrows = 200, colClasses = c("NULL", NA, "NULL", NA, "NULL", NA))
+aux <- read.csv(paste(getwd(), "/Data/songlyrics/lyrics.csv", sep = ""), header = TRUE, sep = ",", nrows = 200, colClasses = c("NULL", NA, "NULL", NA, "NULL", NA))
 #mismo orden de columas que ASCL1
 aux <- aux[,c(2, 1, 3)]
 #concatenar los data frames
-ASCL <- rbind(ASCL, aux)
+ASCL<- rbind(ASCL, aux)
 rm(aux)
 # Convertimos a minúsculas cada palabra, no solo de la letra de las canciones, sino del nombre del grupo y canción
 #Problema -> Al hacer apply devuelve una lista en vez de un data.frame, así que volvemos a convertirlo a data.frame
 # la función t() devuelve la matriz transpuesta, ya que por alguna razón apply devuelve las columnas como filas y al revés
-
-ASCL <- data.frame(t(apply(ASCL, 1, tolower))) #stopwords estan en minuscula
-ASCL[,3] <- removeWords(as.character(ASCL[,3]), words = c(stopwords("english"), "oh", "ah", "eh", "uh", "ma")) 
+ASCL <- data.frame(ASCL)
+ASCL <- setNames(ASCL <- data.frame(ASCL[[1]],apply(ASCL[2:3], 2, tolower)), c("artist","song","lyrics")) #no transformamos la columna de artistas
+ASCL[,3] <- removeWords(as.character(ASCL[,3]), words = c(stopwords("english"), "oh", "ah", "eh", "uh", "ma"))  #stopwords estan en minuscula
 ASCL[,3] <- stripWhitespace(ASCL[,3])
 ASCL[,3] <- removePunctuation(ASCL[,3])
 ASCL[,3] <- removeNumbers(ASCL[,3]) # Por si acaso
@@ -61,7 +65,7 @@ words_data <- cbind.data.frame(names(words.freq),as.integer(words.freq)) #unimos
 names(words_data) <- c("word", "repetitions")
 words_data <- words_data[order(words_data$repetitions, decreasing = TRUE)[1:10], ] #Cogemos las 10 palabras con más apariciones
 ### Get words from a certain band
-queen_songs <- ASCL[ASCL[1] == "queen",,]
+queen_songs <- ASCL[ASCL[1] == "Queen",,]
 queen_songs <- data.frame(queen_songs[2],queen_songs[3]) #We don't need the band name
 #Choose certain songs to explore
 songs_to_select <- which(queen_songs$song %in% c("love of my life", "somebody to love", "bohemian rhapsody", "killer queen", "the show must go on"))
@@ -103,20 +107,6 @@ barplot(words_data$repetitions,
         las = 2)
 dev.off()
 
-#esta es la idea aunque no funciona:
-#for(i in 1:length(ASCL[[3]])){
-#  words <- unlist(stri_extract_all_words(ASCL[[3]][i]))
-#  lyrics[i] <- words
-#}
-#obtenemos las columnas del dataframe por separado para poder trabajar y crear el hash
-auths <- ASCL[[1]]
-sNames <- ASCL[[2]]
-lyrics <- rep(list("character"), length(ASCL[[3]]))
-#en este ejemplo creamos un hash con clave el nombre de autor
-#y el valor otro hash con clave cancion y valor letra
-h <- hash(ASCL[[2]], ASCL[[3]])
-#un hash como valor por cada autor como clave
-o <- hash(auths, c(h, h, h, h, h))
 ############################################################################
 ################### OBTENCIÓN DEL PAIS DE CADA AUTOR #######################
 install.packages("rvest")
@@ -128,8 +118,13 @@ library(rvest)
 #Además, en Wikipedia siempre está la etiqueta Origen, donde viene el país en último lugar
 #El código funciona para el ejemplo de Queen, tenemos que conseguir que funcione el bucle
 #y crear un nuevo data frame artista - país
-for(artist in ASCL[[1]]){
-  url <- paste("https://es.wikipedia.org/wiki/",artist)
+artist_country <- list() #introducimos aquí el país de cada artista
+artists <- as.data.frame(table(ASCL$artist)) #aquí van los artistas con su frecuencia en el dataframe
+artists[,1] <- str_replace_all(artists[,1],"Of","of") #queda pulir los artículos de los artistas
+artists[,1] <- str_replace_all(artists[,1]," ","_") #en las url los espacios se sustituyen por '_'
+
+for(Var1 in artists[[1]]){
+  url <- paste("https://es.wikipedia.org/wiki/", Var1)
   page <- read_html(url)
   htmltable <- html_table(page)[1]
   data <- as.data.frame(htmltable)[,-2]
@@ -137,13 +132,14 @@ for(artist in ASCL[[1]]){
   origin_info <- data[which(grepl("Origen", data$X1)),2]
   origin_list <- unlist(strsplit(origin_info, ","))
   country <- trimws(origin_list[length(origin_list)])
-  #meter el pais y el artista en un data frame
+  artist_country <- c(artist_country, country)
 }
+artists <- cbind(artists, artist_country)   #meter el pais y el artista en un data frame
+
 ############################################################################
 #VISUALIZACION DE DATOS
-auths_count <- as.data.frame(table(ASCL$artist)) #contabilidad de el numero de veces que aparece cada autor en el dataframe
 plot(auths_count) #visualizacion de los datos antes de agrupar
-
+wordcloud(words_data[,1], freq = words_data[,2],min.freq = 1, random.order = FALSE,color= brewer.pal(8, "Dark2"), max.words = 500)
 #Visualización 10 palabras más utilizadas
 pal <- colorRampPalette(colors = c("blue", "lightblue"))(length(words_data[[1]]))
 barplot(words_data$repetitions, 
